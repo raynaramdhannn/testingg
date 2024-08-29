786ad6fef558b37ea46de6c042b98a36965d7ca5
raynaramdan210 <raynaramdan210@gmail.com>
2024-08-29 03:46:21 +0000

    Merubah Title

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 11 CRUD Application - ItSolutionStuff.com</title>
+    <title>Wahaji Store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 </head>
